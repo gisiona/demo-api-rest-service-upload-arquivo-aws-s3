d4addc54d41267b4f5eb9dd4a1e9ffdb37f5d3af
gisiona <gil-real@hotmail.com>
2023-08-08 17:39:44 -0300

    adicionado projeto inicial para upload de objeto no s3

---
+++ src/main/java/com/gisiona/serviceuploadarquivos3/ServiceUploadArquivoS3Application.java
@@ -0,0 +1,13 @@
+package com.gisiona.serviceuploadarquivos3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceUploadArquivoS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceUploadArquivoS3Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/gisiona/serviceuploadarquivos3/controller/UploadArquivoS3Controller.java
@@ -0,0 +1,39 @@
+package com.gisiona.serviceuploadarquivos3.controller;
+
+import com.gisiona.serviceuploadarquivos3.service.StorageS3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/arquivos")
+public class UploadArquivoS3Controller {
+    @Autowired
+    private StorageS3Service service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = service.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
+}
